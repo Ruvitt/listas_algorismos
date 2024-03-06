a8524ea7ab91f6465b3c1a802e7d30a3cbe1b9df
Ruan Cordeiro <ruan59333@gmail.com>
2024-03-06 14:16:01 -0300

    primeira lista

---
+++ lista1.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Primeira questão
+    // int idade, meses, dias;
+    // cout << "Digite sua idade: ";
+    // cin >> idade;
+
+    // cout << "Digite seus meses: ";
+    // cin >> meses;
+
+    // cout << "digite a quantidade de dias: ";
+    // cin >> dias;
+
+    // cout << "Sua idade em dias é: " << (idade * 365) + (meses * 30) + dias;
+
+    // Segunda questão
+    // int anos_fumando, cigarros_por_dia, preco_carteira;
+    
+    // cout << "Digite a quantidade de anos que você fuma: ";
+    // cin >> anos_fumando;
+
+    // cout << "Digite a quantidade de cigarros que você fuma por dia: ";
+    // cin >> cigarros_por_dia;
+
+    // cout << "Digite o preço da carteira de cigarro: ";
+    // cin >> preco_carteira;
+
+    // int cigarros = (anos_fumando * 365) * cigarros_por_dia;
+    
+    // cout << "Você fumou " << cigarros << " e gastou " << cigarros * preco_carteira << " reais " << "durante um periodo de " << anos_fumando << " anos.";
+
+    // Terceira questão
+    // int valor_compra;
+
+    // cout << "Digite o valor da compra: ";
+    // cin >> valor_compra;
+
+    // cout << "O valor comprando a vista é " << valor_compra << " e parcelando em 10 vezes fica " << valor_compra / 10 << " reais";
+
+    // Quarta questão
+    // int numero = 95;
+    // float percentual = 0.65; 
+
+    // cout << "O valor do percentual aplicado ao numero é: " << numero * percentual;
+
+    // Quinta questão
+    // int segundos = 12455;
+    // int horas = segundos / 3600;
+    // int minutos = (segundos / 60) % 60;
+    // int segundos_restantes = segundos % 60;
+
+    // cout << "O valor de " << segundos << " em horas, minutos e segundos é: " << horas << " horas, " << minutos << " minutos e " << segundos_restantes << " segundos.";
+
+    // Sexta questão
+    // int custo_carro, valor_distribuidor, valor_imposto;
+
+    // cout << "Digite o valor do carro: ";
+    // cin >> custo_carro;
+
+    // valor_distribuidor = custo_carro * 0.28;
+    // valor_imposto = custo_carro * 0.45;
+
+    // cout << "O valor do carro com o valor do distribuidor e os impostos fica: " << custo_carro + valor_distribuidor + valor_imposto;
+
+    // Setima questâo 
+    // int temp_celsius;
+    // int temp_fehrenheit;
+
+    // cout << "Digite a temperatura em celsius: ";
+    // cin >> temp_celsius;
+    
+    // temp_fehrenheit = (9*temp_celsius+160) / 5;
+    
+    // cout << "Essa temperatura em fahrenheit é: " << temp_fehrenheit;
+
+    // Oitava questão 
+    // float valor_cotacao;
+    // int valor_dolar;
+
+    // cout << "Digite a cotação do dolar atual: ";
+    // cin >> valor_cotacao;
+
+    // cout << "Digite a quantia em dolares que deseja converter: ";
+    // cin >> valor_dolar;
+
+    // cout << "A quantia em reais é: " << valor_dolar * valor_cotacao;
+
+    // Nona questão  
+    // int val1, val2;
+
+    // cout << "Digite o primeiro valor: ";
+    // cin >> val1;
+
+    // cout << "Digite o segundo valor: ";
+    // cin >> val2;
+
+    // int temp = val1;
+    // val1 = val2;
+    // val2 = temp;
+
+    // cout << "O valor da primeira variavel foi: " << val1 << "\nO valor da segunda variavel foi: " << val2;
+
+    // Decima questâo
+    // int a, b;
+    // bool c = true;
+    // bool d = false;
+
+    // cout << "Digite o primeiro valor: ";
+    // cin >> a;
+
+    // cout << "Digite o segundo valor: ";
+    // cin >> b;
+
+    // cout << "Os resultados foram:\nAdição: " << a + b << "\nSubtração: " << a - b << "\nMultiplicação: " << a * b << "\nDivisão: " << a / b << "\n";
+
+    // cout << "Os resultados das operações lógicas foram:\nAND: " << (c && d) << "\nOR: " << (c || d) << "\nNOT c: " << !c << "\nNOT d: " << !d << "\n";
+}
